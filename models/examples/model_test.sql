52d77bc66cb9d14ddf9b390799166c42c0fce60c
Alexander Fokin <developer1@gmail.com>
2023-02-16 04:15:59 +0000

    update

--- models/examples/model_test.sql
+++ models/examples/model_test.sql
@@ -3,7 +3,8 @@ with countries as (
     select 
     country_name as country,
     country_code as code
-    FROM {{ source("test", "country_region") }}
+    --FROM {{ source("test", "country_region") }}
+    FROM `dbt_srangelp.country_region`
 )
 
-SELECT * FROM countries;
+SELECT * FROM countries
